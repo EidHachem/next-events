c48d76b0fd5085d83a1d5225a66e7d249dd1fa25
eid <eidhachem1@gmail.com>
2024-10-15 21:17:26 +0300

    Add error alert ui component

--- pages/events/[...slug]/index.js
+++ pages/events/[...slug]/index.js
@@ -1,5 +1,7 @@
 import EventList from "@/components/events/event-list"
 import ResultsTitle from "@/components/events/results-title/results-title"
+import Button from "@/components/ui/button"
+import ErrorAlert from "@/components/ui/error-alert/error-alert"
 import { getFilteredEvents } from "@/dummy-data"
 import { useRouter } from "next/router"
 import React from "react"
@@ -28,13 +30,31 @@ const FilteredEvents = () => {
     numMonth > 12 ||
     numMonth < 1
   ) {
-    return <p>Invalid filter, Please adjust your values!</p>
+    return (
+      <>
+        <ErrorAlert>
+          <p>Invalid filter, Please adjust your values!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    )
   }
 
   const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth })
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <p>No events found!</p>
+    return (
+      <>
+        <ErrorAlert>
+          <p>No events found!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </>
+    )
   }
 
   const date = new Date(numYear, numMonth - 1)
