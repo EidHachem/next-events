b707f3cc240ddbbc02b44b607123c94806b77045
eid <eidhachem1@gmail.com>
2024-10-15 20:57:40 +0300

    Add functionality to event filtering form

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
 import EventList from "@/components/events/event-list"
 import EventSearch from "@/components/events/event-search"
 import { getAllEvents } from "@/dummy-data"
+import { useRouter } from "next/router"
 
 const EventsPage = () => {
+  const router = useRouter()
   const events = getAllEvents()
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`
+    router.push(fullPath)
+  }
 
   return (
     <>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandler} />
       <EventList events={events} />
     </>
   )
