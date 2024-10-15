6279c0d241503e1af32be7c2633504704666c3b8
eid <eidhachem1@gmail.com>
2024-10-15 21:12:31 +0300

    Add filtering logic

--- pages/events/[...slug]/index.js
+++ pages/events/[...slug]/index.js
@@ -1,7 +1,50 @@
+import EventList from "@/components/events/event-list"
+import ResultsTitle from "@/components/events/results-title/results-title"
+import { getFilteredEvents } from "@/dummy-data"
+import { useRouter } from "next/router"
 import React from "react"
 
 const FilteredEvents = () => {
-  return <div>FilteredEvents</div>
+  const router = useRouter()
+
+  const filteredData = router.query.slug
+
+  if (!filteredData) {
+    return <p className="center">Loading...</p>
+  }
+
+  const [filteredYear, filteredMonth] = filteredData
+
+  const numYear = +filteredYear
+  const numMonth = +filteredMonth
+
+  console.log(numMonth)
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth > 12 ||
+    numMonth < 1
+  ) {
+    return <p>Invalid filter, Please adjust your values!</p>
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth })
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found!</p>
+  }
+
+  const date = new Date(numYear, numMonth - 1)
+
+  return (
+    <>
+      <ResultsTitle date={date} />
+      <EventList events={filteredEvents} />
+    </>
+  )
 }
 
 export default FilteredEvents
