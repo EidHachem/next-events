ddc8d9c4db4232f2e3292d68b3f95b674ab11740
eid <eidhachem1@gmail.com>
2024-10-15 20:09:55 +0300

    Add Icons

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,7 +1,8 @@
-import Link from "next/link"
-
 import classes from "@/styles/events/event-item.module.css"
 import Button from "../ui/button"
+import DateIcon from "../icons/date-icon"
+import AddressIcon from "../icons/address-icon"
+import ArrowRightIcon from "../icons/arrow-right-icon"
 
 const EventItem = ({ title, image, date, location, id }) => {
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -21,14 +22,21 @@ const EventItem = ({ title, image, date, location, id }) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
