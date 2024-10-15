bc304652b18663a1f91cff6daa88b2f766d3ece0
eid <eidhachem1@gmail.com>
2024-10-15 20:37:32 +0300

    Add all events page content

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,14 @@
-import React from "react"
+import EventList from "@/components/events/event-list"
+import { getAllEvents } from "@/dummy-data"
 
 const EventsPage = () => {
-  return <div>EventsPage</div>
+  const events = getAllEvents()
+
+  return (
+    <div>
+      <EventList events={events} />
+    </div>
+  )
 }
 
 export default EventsPage
